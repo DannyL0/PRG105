fee7c5f5085d63dd2272f55ba7a7b3e74ce79cc3
DannyL0 <111381164+DannyL0@users.noreply.github.com>
2022-08-24 14:17:11 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+
+month = input("Enter a month: ")
+color = input("Enter a color: ")
+flower = input("Enter a type of flower: ")
+verb = input("Enter a verb: ")
+verb1 = input("Enter another verb: ")
+language = input("Enter a language: ")
+mythical_creature = input("Enter the name of a mythical creature: \n")
+# NOTE new line to separate from final print
+
+
+print(f"Soft benediction of {month} sun;")
+print(f"Voices of {mythical_creature}, laughing as they run;")
+print(f"{color} {language} lawns, bright {flower} and butterflies;")
+print(f"And over all the blue embracing skies.\n ")
+
+print(f"Tumult and {verb} of the incessant gun;")
+print(f"Dead men and dying, trenches lost and won;")
+print(f"Blood, mud, and havoc, bugles, {verb1}, cries;")
+print(f"And over all the {color} embracing skies.")
