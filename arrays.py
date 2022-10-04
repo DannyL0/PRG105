d1082bd73688a944de9b61f7114c58fbbec2cd36
DannyL0 <111381164+DannyL0@users.noreply.github.com>
2022-10-03 20:00:34 -0500

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,23 @@
+alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+         'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+         'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+         'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', ',', '.', '!']
+
+code = ['R', 'j', 'v', 'm', 'F', 'x', '_', 'V', '?', 'f', 'k', '2', ';', '3', '&', '4', '>', '5', '6', '7',
+        '8', '9', '|', ',', "'", '.', '!', 'A', '^' 'B', ':', 'C', '$', 'D', 'p' 'E', 'F', '1', 'G', '%', 'I',
+        '*', 'o', 'n', '0', '~', 'B', 'q', '/', '}', '`', '{', 'v', '@', 'W', 'S', ']',
+        'Z', '#', '+', 'h', '[', 'z', ')', '-', '<', '(', 'L']
+
+encryption = []  # empty array for the results
+
+phrase = input("Enter a phrase to encode:")
+
+for position in phrase:
+    encryption.append(code[alpha.index(position)])
+
+print(encryption)
+
+f = open("encryption.txt", "w")
+for character in encryption:
+    f.write(character + "\n")
+f.close()
