5b640a01b9f5d485b3d505bbb3eb20a00d5acc0a
DannyL0 <111381164+DannyL0@users.noreply.github.com>
2022-11-13 14:48:49 -0600

    Add files via upload

--- crud.py
+++ crud.py
@@ -1,32 +1,36 @@
+import pickle
+
 LOOK_UP = 1
 ADD = 2
 CHANGE = 3
 DELETE = 4
 QUIT = 5
-info_dict = {}
 
 
 def main():
 
-    file = open("customer-file", "r+")
-    file.close()
+    try:
+        file = open("customer-file", "rb")
+        info_dict = pickle.load(file)
+    except(FileNotFoundError, IOError):
+        print("File not found")
+        info_dict = {}
 
-    while True:
-        menu()
-        choice = int(input("Enter your selection: "))
+    choice = 0
+
+    while choice != QUIT:
+        choice = menu()
+
         if choice == LOOK_UP:
-            read()
+            read(info_dict)
         elif choice == ADD:
-            create()
+            create(info_dict)
         elif choice == CHANGE:
-            update()
+            update(info_dict)
         elif choice == DELETE:
-            delete()
+            delete(info_dict)
         elif choice == QUIT:
-            quit()
-        else:
-            print("Incorrect")
-            break
+            saving(info_dict)
 
 
 def menu():
@@ -35,24 +39,27 @@ def menu():
     print("3. Change a person's email")
     print("4. Delete an entry")
     print("5. Quit")
+    choice = int(input("Enter your selection: "))
+    while choice < 1 or choice > 5:
+        choice = int(input("*Your choice was invalid*\nTry again\nEnter your selection: "))
+    return choice
 
 
-def read():
+def read(info_dict):
     name = input("Enter a name: ")
-    if name in info_dict:
-        print(f"Email: {info_dict[name]}")
-    else:
-        print("Person not found")
+    print(info_dict.get(name, "Person not found"))
 
 
-def create():
+def create(info_dict):
     name = input("Enter a name: ")
     address = input("Enter a email address: ")
-    info_dict[name] = address
-    print(f"Successfully added {info_dict[name]} to the info_dict")
+    if name not in info_dict:
+        info_dict[name] = address
+    else:
+        print("That entry already exists")
 
 
-def delete():
+def delete(info_dict):
     name = input("Enter a name: ")
     if name in info_dict:
         del info_dict[name]
@@ -61,7 +68,7 @@ def delete():
         print("Person is not in the info_dict")
 
 
-def update():
+def update(info_dict):
     name = input("Enter a name: ")
     if name in info_dict:
         address = input("Enter the new email of the person\n")
@@ -69,6 +76,13 @@ def update():
         print("Successfully updated email")
     else:
         print("That name is not in the database")
+
+
+def saving(info_dict):
+    print("The database has been successfully updated")
+    save_file = open('customer-file', 'wb')
+    pickle.dump(info_dict, save_file)
+    save_file.close()
 
 
 if __name__ == '__main__':
